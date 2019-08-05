c5bdb9201b347561d9b9990a5454a4ef67d3f0e4
edevars <e.devars.dev@gmail.com>
2019-08-04 19:35:39 -0500

    :sparkles: Redirecting route root to badges

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use("/badges", badgesRouter);
 //midlewares
 app.use(bodyParser.json());
 
+//redirect
+app.get('/', (req,res)=>{
+  res.redirect('/badges')
+})
+
 const server = app.listen(3000, () => {
   console.log(`Listening in http://localhost:${server.address().port}`);
 });
