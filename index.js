b0da588902e9b64754dc930bc8114c10cc58ba28
edevars <e.devars.dev@gmail.com>
2019-08-05 01:21:12 -0500

    :zap: Enviroment variables config

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const badgesRouter = require("./routes/badges");
 
 const app = express();
 
-app.use("/badges", badgesRouter);
-
 //midlewares
 app.use(bodyParser.json());
+
+app.use("/badges", badgesRouter);
 
 //redirect
 app.get('/', (req,res)=>{
   res.redirect('/badges')
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen(8000, () => {
   console.log(`Listening in http://localhost:${server.address().port}`);
 });
