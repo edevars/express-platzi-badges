709b7d84c5a324a2f78adbacad5b1fc42c7b6d51
edevars <e.devars.dev@gmail.com>
2019-08-05 10:18:01 -0500

    :zap: Adding config port

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const config = {
   dev: process.env.NODE_ENV !== "production",
-  devPort: process.env.PORT,
+  port: process.env.PORT,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const badgesRouter = require("./routes/badges");
+const { config } = require("./config");
 
 const app = express();
+const PORT = config.port;
 
 //midlewares
 app.use(bodyParser.json());
@@ -10,10 +12,10 @@ app.use(bodyParser.json());
 app.use("/badges", badgesRouter);
 
 //redirect
-app.get('/', (req,res)=>{
-  res.redirect('/badges')
-})
+app.get("/", (req, res) => {
+  res.redirect("/badges");
+});
 
-const server = app.listen(8000, () => {
+const server = app.listen(PORT, () => {
   console.log(`Listening in http://localhost:${server.address().port}`);
 });
