2043e1fbad94c02ee5d5fc75f3e0d4550dd0b008
edevars <e.devars.dev@gmail.com>
2019-08-04 19:31:59 -0500

    :zap: Error handling

--- routes/badges.js
+++ routes/badges.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const badgesMocks = require("../utils/mocks/badges");
 const BadgeService = require("../services/badges");
 
 const router = express.Router();
@@ -7,56 +6,77 @@ const router = express.Router();
 //Instanciando la clase del sevicio
 const badgeService = new BadgeService();
 
-router.get("/", async function(req, res) {
-  const badges = await badgeService.getBadges();
+router.get("/", async function(req, res, next) {
+  try {
+    const badges = await badgeService.getBadges();
 
-  res.status(200).json({
-    data: badges,
-    message: "badges listed"
-  });
+    res.status(200).json({
+      data: badges,
+      message: "badges listed"
+    });
+  } catch (error) {
+    next(error);
+  }
 });
-
+req;
 router.get("/:badgeId", async function(req, res) {
   const { badgeId } = req.params;
 
-  const badge = await badgeService.getBadge({ badgeId });
+  try {
+    const badge = await badgeService.getBadge({ badgeId });
 
-  res.status(200).json({
-    data: badge,
-    message: "badge retrieve"
-  });
+    res.status(200).json({
+      data: badge,
+      message: "badge retrieve"
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async function(req, res) {
   const { body: badge } = req;
 
-  const badge = await badgeService.createBadge({ badge });
+  try {
+    const badge = await badgeService.createBadge({ badge });
 
-  res.status(201).json({
-    data: badge,
-    message: "badge created"
-  });
+    res.status(201).json({
+      data: badge,
+      message: "badge created"
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:badgeId", async function(req, res) {
   const { badgeId } = req.params;
   const { body: badge } = req;
-  const badgeUpdated = await badgeService.updateBadge({ badgeId, badge });
 
-  res.status(201).json({
-    data: badgeUpdated,
-    message: "badge updated"
-  });
+  try {
+    const badgeUpdated = await badgeService.updateBadge({ badgeId, badge });
+
+    res.status(201).json({
+      data: badgeUpdated,
+      message: "badge updated"
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:badgeId", async function(req, res) {
   const { badgeId } = req.params;
-  const badge = await badgeService.deleteBadge({ badgeId });
+  try {
+    const badge = await badgeService.deleteBadge({ badgeId });
 
-  res.status(201).json({
-    data: badge,
-    message: "badge deleted"
-  });
+    res.status(201).json({
+      data: badge,
+      message: "badge deleted"
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
