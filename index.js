df53fd04b14415bc8801b64208d9314cfbdb0e46
edevars <e.devars.dev@gmail.com>
2019-08-05 22:25:14 -0500

    :zap: Adding cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const badgesRouter = require("./routes/api/badges");
 const { config } = require("./config");
+const cors = require("cors");
 
 const app = express();
 const PORT = config.port;
 
 //midlewares
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/badges", badgesRouter);
 
